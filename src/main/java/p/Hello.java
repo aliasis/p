b57e34f6f7b2d87aca809c3be1cff5f4980ce276
aliasis <aliasis@user.noreplay.github.com>
2016-09-22 00:58:25 +0800

    hello

---
+++ src/main/java/p/Hello.java
@@ -0,0 +1,16 @@
+package p;
+
+/**
+ * Hello world!
+ *
+ */
+public class Hello {
+    public static void main( String[] args )
+    {
+    	System.out.println(new App().sayHello());
+    }
+
+    public String sayHello() {
+	return "Hello!";
+    }
+}
